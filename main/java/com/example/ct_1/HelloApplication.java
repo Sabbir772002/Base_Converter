76d16dff167ec0476a290fa41d6a0dc3800beb2d
Sabbir772002 <55828290+Sabbir772002@users.noreply.github.com>
2022-11-08 19:42:58 +0600

    Update HelloApplication.java

--- main/java/com/example/ct_1/HelloApplication.java
+++ main/java/com/example/ct_1/HelloApplication.java
@@ -10,7 +10,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 460);
-        stage.setTitle("Hello!");
+        stage.setTitle("Base Converter");
         stage.setScene(scene);
         stage.show();
     }
